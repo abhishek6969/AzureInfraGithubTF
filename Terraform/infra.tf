e81d4eb64e5196008fed6987ae7c52c4ab3b9452
abhishek6969 <sunkaleabhishek@gmail.com>
2024-07-01 08:02:09 +0530

    deploy

--- Terraform/infra.tf
+++ Terraform/infra.tf
@@ -160,4 +160,266 @@ resource "azurerm_automation_hybrid_runbook_worker_group" "lirook-workers" {
   automation_account_name = azurerm_automation_account.lirookAutomation.name
 }
 
+resource "azurerm_monitor_data_collection_rule" "dcr-CT" {
+  name                = "dcr-CT"
+  resource_group_name = azurerm_resource_group.azureInfra.name
+  location            = azurerm_resource_group.azureInfra.location
+
+  data_sources {
+    extension {
+      name           = "CTDataSource-Windows"
+      extension_name = "ChangeTracking-Windows"
+      streams = [
+        "Microsoft-ConfigurationChange",
+        "Microsoft-ConfigurationChangeV2",
+        "Microsoft-ConfigurationData"
+      ]
+      # Default CT configuration for Windows
+      extension_json = <<JSON
+        {
+          "enableFiles": true,
+          "enableSoftware": true,
+          "enableRegistry": true,
+          "enableServices": true,
+          "enableInventory": true,
+          "registrySettings": {
+            "registryCollectionFrequency": 3000,
+            "registryInfo": [
+              {
+                "name": "Registry_1",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Group Policy\\Scripts\\Startup",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_2",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Group Policy\\Scripts\\Shutdown",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_3",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_4",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Active Setup\\Installed Components",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_5",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\ShellEx\\ContextMenuHandlers",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_6",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\Background\\ShellEx\\ContextMenuHandlers",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_7",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\Shellex\\CopyHookHandlers",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_8",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_9",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_10",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_11",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_12",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Internet Explorer\\Extensions",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_13",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Extensions",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_14",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Drivers32",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_15",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows NT\\CurrentVersion\\Drivers32",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_16",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\KnownDlls",
+                "valueName": ""
+              },
+              {
+                "name": "Registry_17",
+                "groupTag": "Recommended",
+                "enabled": false,
+                "recurse": true,
+                "description": "",
+                "keyName": "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\Notify",
+                "valueName": ""
+              }
+            ]
+          },
+          "fileSettings": {
+            "fileCollectionFrequency": 2700
+          },
+          "softwareSettings": {
+            "softwareCollectionFrequency": 1800
+          },
+          "inventorySettings": {
+            "inventoryCollectionFrequency": 36000
+          },
+          "servicesSettings": {
+            "serviceCollectionFrequency": 1800
+          }
+        }
+      JSON
+    }
+    extension {
+      name           = "CTDataSource-Linux"
+      extension_name = "ChangeTracking-Linux"
+      streams = [
+        "Microsoft-ConfigurationChange",
+        "Microsoft-ConfigurationChangeV2",
+        "Microsoft-ConfigurationData"
+      ]
+      # Default CT configuration for Linux
+      extension_json = <<JSON
+        {
+          "enableFiles": true,
+          "enableSoftware": true,
+          "enableRegistry": false,
+          "enableServices": true,
+          "enableInventory": true,
+          "fileSettings": {
+            "fileCollectionFrequency": 900,
+            "fileInfo": [
+              {
+                "name": "ChangeTrackingLinuxPath_default",
+                "enabled": true,
+                "destinationPath": "/etc/.*.conf",
+                "useSudo": true,
+                "recurse": true,
+                "maxContentsReturnable": 5000000,
+                "pathType": "File",
+                "type": "File",
+                "links": "Follow",
+                "maxOutputSize": 500000,
+                "groupTag": "Recommended"
+              }
+            ]
+          },
+          "softwareSettings": {
+            "softwareCollectionFrequency": 300
+          },
+          "inventorySettings": {
+            "inventoryCollectionFrequency": 36000
+          },
+          "servicesSettings": {
+            "serviceCollectionFrequency": 300
+          }
+        }
+      JSON
+    }
+  }
+
+  data_flow {
+    streams = [
+      "Microsoft-ConfigurationChange",
+      "Microsoft-ConfigurationChangeV2",
+      "Microsoft-ConfigurationData"
+    ]
+    destinations = [
+      "Microsoft-CT-Dest"
+    ]
+  }
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = azurerm_log_analytics_workspace.test-law-lirook.id
+      name                  = "Microsoft-CT-Dest"
+    }
+  }
+}
+
 
